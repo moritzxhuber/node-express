e44b7b0e9d11b121c1229a299570338b9d5d000d
Moritz Huber <moritz.huber11@gmx.de>
2020-08-09 17:11:16 +0200

    Finished Week 1 Workshop Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -17,7 +17,7 @@ campsiteRouter
   })
   .post((req, res) => {
     res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+      `Will add the campsite "${req.body.name}" with description "${req.body.description}"`
     );
   })
   .put((req, res) => {
@@ -26,6 +26,29 @@ campsiteRouter
   })
   .delete((req, res) => {
     res.end("Deleting all campsites");
+  });
+
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Will send campsite ${req.params.campsiteId} to you`);
+  })
+  .post((req, res) => {
+    res.end(`POST operation not supported on /campsites/:campsiteId`);
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `Will update campsite ${req.params.campsiteId} with "${req.body.name}" and "${req.body.description}" for you`
+    );
+  })
+  .delete((req, res) => {
+    res.end(`Deleting campsite ${req.params.campsiteId}`);
   });
 
 module.exports = campsiteRouter;
